90a5488307f3b329a1f444549dc6d13f74b550b8
Marie Auger-Methe <Marie@t9366.wpa.dal.ca>
2014-10-08 10:56:22 -0300

    Change from -pi-pi to 0-2pi
    Small changes in terms of angles format and used the unit circle
    instead of random points

--- Cartesian2Polar.R
+++ Cartesian2Polar.R
@@ -4,11 +4,11 @@
 # These could be position in terms of Northing and Easting
 
 # Number of points to simulate
-set.seed(764)
-n <- 6
-cartCoord <- cbind(rnorm(n,5,6),rnorm(n,0,6))
+cartCoord <- cbind(c(1,sqrt(0.5),0,-sqrt(0.5),-1,-sqrt(0.5),0,sqrt(0.5)),
+                   c(0,sqrt(0.5),1,sqrt(0.5),0,-sqrt(0.5),-1,-sqrt(0.5)))
 colnames(cartCoord) <- c("x","y")
-plot(cartCoord, pch=20, xlim=c(-10,25), ylim=c(-12,12))
+par(pty="s", mar=c(5,3,1,1))
+plot(cartCoord, pch=20, xlim=c(-2,2), ylim=c(-2,2))
 # Create a text version to display on plot
 cartCoordText <- paste("(", round(cartCoord[,1],1), ", ", round(cartCoord[,2],1), ")", sep="")
 text(cartCoord[,1],cartCoord[,2],labels=cartCoordText, cex=0.6, pos=2)
@@ -23,10 +23,20 @@ r <- sqrt(rowSums(cartCoord^2))
 # In Quadrant I (+ve x and +ve y)
 # tan(theta) = y/x
 # theta = arctan(y/x)
-# In R atan2(y,x) gives the angle between the x axis and the vector
+# In R atan2(x,y) gives the angle between the y axis and the vector
 # from origin to (x,y) in radian
-theta <- atan2(cartCoord[,2], cartCoord[,1])
+# Y-axis can be interpreted as North
+# Thus positive angles represent a turn to the east
+# and negative angle represent a turn to the west.
+theta <- atan2(cartCoord[,1],cartCoord[,2])
 
 polarCoord <- cbind(r,theta) # In radian
 polarCoordText <- paste("(", round(r,1), ", ", round(theta,1), ")", sep="")
 text(cartCoord[,1],cartCoord[,2],labels=polarCoordText, cex=0.6, pos=4, col="red")
+
+# If you want the turn from 0 -2*pi, instead of -pi-pi
+theta2pi <- theta + ifelse(theta<0,2*pi,0)
+polarCoord2piText <- paste("(", round(r,1), ", ", round(theta2pi,1), ")", sep="")
+text(cartCoord[,1],cartCoord[,2],labels=polarCoord2piText, cex=0.6, pos=1, col="blue")
+
+
